048ee0d5a782dd1c9428e6766edb0670d253635c
[OMKUMAR MAURYA] <[mauryaomkumar199@gmail.com]>
2024-12-01 17:38:16 +0530

    Setup.py file Ready

---
+++ setup.py
@@ -0,0 +1,69 @@
+'''
+The setup.py file is an essential part of packaging and 
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
+of your project, such as its metadata, dependencies, and more
+'''
+
+# from setuptools import find_packages,setup
+# from typing import List
+
+# def get_requirements()->List[str]:
+#     """
+#     Thiss function will return list of requirements
+    
+#     """
+#     requirement_lst:List[str]=[]
+#     try:
+#         with open('requirements.txt','r') as file:
+#             #Read lines from the file
+#             lines=file.readlines()
+#             ## Process each line
+#             for line in lines:
+#                 requirement=line.strip()
+#                 ## ignore empty lines and -e .
+#                 if requirement and requirement!= '-e .':
+#                     requirement_lst.append(requirement)
+#     except FileNotFoundError:
+#         print("requirements.txt file not found")
+
+#     return requirement_lst
+# # print(get_requirements())
+
+# setup(
+#     name="NetworkSecurity",
+#     version="0.0.1",
+#     author="Omkumar",
+#     author_email="mauryaomkumar199@gmail.com",
+#     packages=find_packages(),
+#     install_requires=get_requirements()
+# )
+
+
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements() -> List[str]:
+    """
+    This function reads the requirements.txt file and returns a list of valid package names.
+    """
+    requirement_lst: List[str] = []
+    try:
+        with open('requirements.txt', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and not requirement.startswith('-e'):
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+    return requirement_lst
+
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Omkumar",
+    author_email="mauryaomkumar199@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements()
+)
